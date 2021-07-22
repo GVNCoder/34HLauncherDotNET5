0e9790f5244125145c6b7e18a220e5587d4ca1bd
DmitrievStanislav <dmitrievsvp@gmail.com>
2021-07-22 22:16:26 +0300

    Added Commands + Editor config
    * Added RelayCommand impl
    * Added AsyncRelayCommand impl

---
+++ Launcher/Launcher.App/Commands/AsyncRelayCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace Launcher.Commands
+{
+    // https://mike-ward.net/2013/08/09/asynccommand-implementation-in-wpf/
+    public class AsyncRelayCommand<T> : ICommand
+    {
+        private readonly Func<T, Task> _executeDelegate;
+        private readonly Func<T, bool> _canExecuteDelegate;
+
+        private bool _isExecuting;
+
+        #region Ctor
+
+        public AsyncRelayCommand(Func<T, Task> executeDelegate, Func<T, bool> canExecuteDelegate = null)
+        {
+            _executeDelegate = executeDelegate ?? throw new ArgumentNullException(nameof(executeDelegate));
+            _canExecuteDelegate = canExecuteDelegate;
+        }
+
+        #endregion
+
+        #region ICommand
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+            => _isExecuting == false && (_canExecuteDelegate?.Invoke((T) parameter) ?? true);
+
+        public async void Execute(object parameter)
+        {
+            // set internal state as busy
+            _isExecuting = true;
+
+            // trigger refresh CanExecute
+            OnCanExecuteChanged();
+            
+            try
+            {
+                await _executeDelegate((T) parameter);
+            }
+            finally
+            {
+                _isExecuting = false;
+                OnCanExecuteChanged();
+            }
+        }
+        public void OnCanExecuteChanged()
+            => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        #endregion
+    }
+}
---
+++ Launcher/Launcher.App/Commands/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Launcher.Commands
+{
+    // https://jebarson.dev/blog/writing-an-asynchronous-relaycommand-implementing-icommand/
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _executeDelegate;
+        private readonly Func<T, bool> _canExecuteDelegate;
+
+        #region Ctor
+
+        public RelayCommand(Action<T> executeDelegate, Func<T, bool> canExecuteDelegate = null)
+        {
+            _executeDelegate = executeDelegate ?? throw new ArgumentNullException(nameof(executeDelegate));
+            _canExecuteDelegate = canExecuteDelegate;
+        }
+
+        #endregion
+
+        #region ICommand
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => _canExecuteDelegate?.Invoke((T) parameter) ?? true;
+        public void Execute(object parameter) => _executeDelegate((T) parameter);
+        public void OnCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        #endregion
+    }
+}
