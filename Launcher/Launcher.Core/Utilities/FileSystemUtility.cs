39fae20505353b032a141ae900af088157e7137c
DmitrievStanislav <dmitrievsvp@gmail.com>
2021-07-22 16:15:45 +0300

    Created FileSystemUtility

---
+++ Launcher/Launcher.Core/Utilities/FileSystemUtility.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace Launcher.Core.Utilities
+{
+    public static class FileSystemUtility
+    {
+        // https://stackoverflow.com/questions/278439/creating-a-temporary-directory-in-windows
+        public static string GetTemporaryDirectory()
+        {
+            // generate temp directory path
+            var tempPath = Path.GetTempPath();
+            var tempFileName = Path.GetRandomFileName();
+            var tempDirectory = Path.Combine(tempPath, tempFileName);
+
+            // generate physical directory
+            Directory.CreateDirectory(tempDirectory);
+
+            return tempDirectory;
+        }
+    }
+}
