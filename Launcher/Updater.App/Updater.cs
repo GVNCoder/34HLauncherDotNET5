b5e7702dc936c68380b1df2798f1f3468da5fefb
DmitrievStanislav <dmitrievsvp@gmail.com>
2021-08-03 16:14:41 +0300

    Update Updater.cs

--- Launcher/Updater.App/Updater.cs
+++ Launcher/Updater.App/Updater.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable InvertIf
+// ReSharper disable RedundantAssignment
+// ReSharper disable InlineOutVariableDeclaration
+
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -8,18 +13,68 @@ using Updater.App.Models;
 
 namespace Updater.App
 {
-    public static class Updater
+    public class Updater
     {
+        #region Constants
+
         private const int RunArgumentsIndex = 1;
+
+        #endregion
+
+        private UpdaterRunArguments _runArguments;
+        private string[] _deleteFiles;
+
+        #region Ctor
+
+        private Updater(UpdaterRunArguments runArguments)
+        {
+            _runArguments = runArguments;
+        }
+
+        #endregion
 
         public static void Main(string[] args)
         {
             // get run arguments
             var rawArguments = args[RunArgumentsIndex];
-            var arguments = JsonConvert.DeserializeObject<UpdaterRunArguments>(rawArguments);
+            var runArguments = JsonConvert.DeserializeObject<UpdaterRunArguments>(rawArguments);
 
-            // close launcher
-            var processName = Path.GetFileNameWithoutExtension(arguments.ProcessBackPath);
+            // create an application instance
+            var updater = new Updater(runArguments);
+            var errorMessage = string.Empty;
+
+            updater.Initialize();
+
+            // step 1 Close process and wait it
+            updater.CloseAndWaitTargetProcess();
+
+            // step 2 Verify delete file list
+            updater.VerifyFilesAvailability(out errorMessage);
+            if (string.IsNullOrEmpty(errorMessage) == false)
+            {
+                updater.FinalizeUpdater(errorMessage);
+            }
+
+            // step 3 Delete files
+            updater.DeleteFiles();
+
+            // step 4 Move files
+            updater.MoveFiles();
+
+            // step 5 Run process back
+            updater.FinalizeUpdater(errorMessage);
+        }
+
+        private void Initialize()
+        {
+            _deleteFiles = File.ReadAllLines(_runArguments.DeleteListFileName)
+                .Where(s => string.IsNullOrWhiteSpace(s) == false)
+                .ToArray();
+        }
+
+        private void CloseAndWaitTargetProcess()
+        {
+            var processName = Path.GetFileNameWithoutExtension(_runArguments.ProcessBackPath);
             var process = Process.GetProcessesByName(processName)
                 .FirstOrDefault();
 
@@ -35,15 +90,15 @@ namespace Updater.App
 
                 waitForProcessExistEvent.WaitOne();
             }
+        }
 
-            // verify access to file and remove it
-            var filesToDelete = File.ReadAllLines(arguments.DeleteListFileName)
-                .Where(s => string.IsNullOrWhiteSpace(s) == false)
-                .ToArray();
+        private void VerifyFilesAvailability(out string errorMessage)
+        {
+            errorMessage = string.Empty;
 
             try
             {
-                foreach (var filePath in filesToDelete)
+                foreach (var filePath in _deleteFiles)
                 {
                     var file = new FileInfo(filePath);
                     if (file.Exists)
@@ -57,33 +112,39 @@ namespace Updater.App
             }
             catch (Exception exception)
             {
-                FinalizeUpdater(arguments.ProcessBackPath, $"Delete list exception type {exception.GetType().Name}. Message {exception.Message}");
+                errorMessage = $"{nameof(VerifyFilesAvailability)} exception {exception.GetType().Name}. Message {exception.Message}";
             }
+        }
 
-            foreach (var filePath in filesToDelete)
+        private void DeleteFiles()
+        {
+            foreach (var filePath in _deleteFiles)
             {
                 File.Delete(filePath);
             }
-
-            //var filesToMove = Directory.GetFiles(arguments.UpdateFilesDirPath, "*.*", SearchOption.TopDirectoryOnly);
-            var moveToDir = Path.GetDirectoryName(arguments.ProcessBackPath);
-
-            Directory.Move(arguments.UpdateFilesDirPath, moveToDir);
-
-            FinalizeUpdater(arguments.ProcessBackPath, null);
         }
 
-        #region Private helpers
-
-        private static void FinalizeUpdater(string processBackPath, string errorMessage)
+        private void MoveFiles()
         {
-            // run process back
-            var currentProcess = Process.GetCurrentProcess();
-            var mainModule = currentProcess.MainModule;
-            var currentExecutablePath = mainModule?.FileName;
-            var currentWorkingDirectory = Path.GetDirectoryName(currentExecutablePath);
+            var sourceDirectory = _runArguments.UpdateFilesDirPath;
+            var destinationDirectory = Path.GetDirectoryName(_runArguments.ProcessBackPath);
+            var sourceDirectoryFiles =
+                Directory.EnumerateFiles(sourceDirectory, "*.*", SearchOption.TopDirectoryOnly);
 
-            var workingDirectory = Path.GetDirectoryName(processBackPath);
+            foreach (var sourceDirectoryFile in sourceDirectoryFiles)
+            {
+                Directory.Move(sourceDirectoryFile, destinationDirectory);
+            }
+        }
+
+        private void FinalizeUpdater(string errorMessage)
+        {
+            var currentAppDomain = AppDomain.CurrentDomain;
+            var currentWorkingDirectory = currentAppDomain.BaseDirectory;
+            var currentExecutablePath = Path.Combine(currentWorkingDirectory, currentAppDomain.FriendlyName);
+
+            // run process back
+            var workingDirectory = Path.GetDirectoryName(_runArguments.ProcessBackPath);
             var arguments = JsonConvert.SerializeObject(new
                 { updaterFileName = currentExecutablePath, updateDirPath = currentWorkingDirectory, errorMessage });
 
@@ -91,7 +152,7 @@ namespace Updater.App
             {
                 StartInfo = new ProcessStartInfo
                 {
-                    FileName = processBackPath,
+                    FileName = _runArguments.ProcessBackPath,
                     WorkingDirectory = workingDirectory,
                     Arguments = arguments
                 }
@@ -102,7 +163,5 @@ namespace Updater.App
             // stop current execution
             Environment.Exit(0);
         }
-
-        #endregion
     }
 }
