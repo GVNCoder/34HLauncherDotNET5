676c197f6243a06368db6f04df646c34076bfc0d
DmitrievStanislav <dmitrievsvp@gmail.com>
2021-08-16 19:29:01 +0300

    (Draft) Implemented non client window part

--- Launcher/Launcher.App/ViewModels/MainWindowView/WindowNonClientViewModel.cs
+++ Launcher/Launcher.App/ViewModels/MainWindowView/WindowNonClientViewModel.cs
@@ -1,8 +1,71 @@
-﻿using System.Windows;
+﻿// ReSharper disable MemberCanBePrivate.Global
+
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+using Launcher.Commands;
+using Launcher.Views;
 
 namespace Launcher.ViewModels
 {
     public class WindowNonClientViewModel : DependencyObject
     {
+        private readonly Window _windowRef;
+
+        private bool _isWindowCanBeRestored;
+
+        #region Ctor
+
+        public WindowNonClientViewModel()
+        {
+            _windowRef = Application.Current.Windows
+                .OfType<MainWindowView>()
+                .First();
+
+            // track window state
+            _windowRef.StateChanged += (sender, args) => _isWindowCanBeRestored = _windowRef.WindowState == WindowState.Maximized;
+
+            // create commands
+            SystemWindowCloseCommand = new RelayCommand<object>(_SystemWindowCloseExecuteCommand);
+            SystemWindowMinimizeCommand = new RelayCommand<object>(_SystemWindowMinimizeExecuteCommand);
+            SystemWindowMaximizeCommand = new RelayCommand<object>(_SystemWindowMaximizeExecuteCommand);
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand SystemWindowCloseCommand { get; }
+
+        private void _SystemWindowCloseExecuteCommand(object args)
+        {
+            SystemCommands.CloseWindow(_windowRef);
+        }
+
+        public ICommand SystemWindowMinimizeCommand { get; }
+
+        private void _SystemWindowMinimizeExecuteCommand(object args)
+        {
+            SystemCommands.MinimizeWindow(_windowRef);
+        }
+
+        public ICommand SystemWindowMaximizeCommand { get; }
+
+        private void _SystemWindowMaximizeExecuteCommand(object args)
+        {
+            if (_isWindowCanBeRestored)
+            {
+                SystemCommands.RestoreWindow(_windowRef);
+                _isWindowCanBeRestored = false;
+            }
+            else
+            {
+                SystemCommands.MaximizeWindow(_windowRef);
+                _isWindowCanBeRestored = true;
+            }
+        }
+
+        #endregion
     }
 }
---
+++ Launcher/Launcher.App/Views/MainWindow/WindowNonClientControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Launcher.Views
+{
+    /// <summary>
+    /// Interaction logic for WindowNonClientControl.xaml
+    /// </summary>
+    public partial class WindowNonClientControl : UserControl
+    {
+        public WindowNonClientControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
