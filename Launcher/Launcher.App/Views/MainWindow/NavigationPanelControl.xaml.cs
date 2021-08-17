09109eb538031152678294c4a350b780181b4284
DmitrievStanislav <dmitrievsvp@gmail.com>
2021-08-17 19:31:53 +0300

    Added NavigationPanel

---
+++ Launcher/Launcher.App/Views/MainWindow/NavigationPanelControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Launcher.Views
+{
+    /// <summary>
+    /// Interaction logic for NavigationPanelControl.xaml
+    /// </summary>
+    public partial class NavigationPanelControl : UserControl
+    {
+        public NavigationPanelControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
