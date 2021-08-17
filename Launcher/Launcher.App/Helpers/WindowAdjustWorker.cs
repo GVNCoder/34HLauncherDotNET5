1fbe45e655c12a9024bb84ab0aa3537fbef191e1
DmitrievStanislav <dmitrievsvp@gmail.com>
2021-08-17 13:12:46 +0300

    Moved window border fix logic to WindowAdjustWorker

--- Launcher/Launcher.App/Helpers/WindowAdjustWorker.cs
+++ Launcher/Launcher.App/Helpers/WindowAdjustWorker.cs
@@ -1,6 +1,9 @@
 ﻿// ReSharper disable SwitchStatementMissingSomeCases
+// ReSharper disable InconsistentNaming
+// ReSharper disable IdentifierTypo
 
 using System;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Shell;
 
@@ -10,6 +13,45 @@ namespace Launcher.Helpers
 {
     public class WindowAdjustWorker
     {
+        #region Constants
+
+        private const float DefaultWPFDPI = 96f;
+
+        #endregion
+
+        #region External imports
+
+        [DllImport("user32.dll")]
+        private static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("gdi32.dll")]
+        private static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int GetSystemMetrics(GetSystemMetricsIndex nIndex);
+
+        #endregion
+
+        #region Internal types
+
+        private enum GetDeviceCapsIndex
+        {
+            LOGPIXELSX = 88,
+            LOGPIXELSY = 90
+        }
+
+        private enum GetSystemMetricsIndex
+        {
+            CXFRAME = 32,
+            CYFRAME = 33,
+            SM_CXPADDEDBORDER = 92
+        }
+
+        #endregion
+
         private readonly Window _window;
         private readonly WindowChrome _windowChrome;
 
@@ -53,12 +95,49 @@ namespace Launcher.Helpers
                     _window.BorderThickness = _defaultBorderThickness;
                     break;
                 case WindowState.Maximized:
-                    var (left, top, right, bottom) = WindowResizeBorderFix.GetWindowResizeBorderThickness();
+                    var (left, top, right, bottom) = _GetWindowResizeBorderThickness();
 
                     _window.BorderThickness = new Thickness(left, top, right, bottom);
                     _windowChrome.CaptionHeight = _maximizedCaptionHeight;
                     break;
             }
+        }
+
+        private static (float left, float top, float right, float bottom) _GetWindowResizeBorderThickness()
+        {
+            var dpiX = _GetDpi(GetDeviceCapsIndex.LOGPIXELSX);
+            var dpiY = _GetDpi(GetDeviceCapsIndex.LOGPIXELSY);
+
+            var dx = GetSystemMetrics(GetSystemMetricsIndex.CXFRAME);
+            var dy = GetSystemMetrics(GetSystemMetricsIndex.CYFRAME);
+
+            // this adjustment is needed only since .NET 4.5 
+            var d = GetSystemMetrics(GetSystemMetricsIndex.SM_CXPADDEDBORDER);
+
+            dx += d;
+            dy += d;
+
+            var leftBorder = dx / dpiX;
+            var topBorder = dy / dpiY;
+
+            return (leftBorder, topBorder, leftBorder, topBorder);
+        }
+
+        private static float _GetDpi(GetDeviceCapsIndex index)
+        {
+            var desktopWnd = IntPtr.Zero;
+            var dc = GetDC(desktopWnd);
+
+            float dpi;
+            try
+            {
+                dpi = GetDeviceCaps(dc, (int)index);
+            }
+            finally
+            {
+                ReleaseDC(desktopWnd, dc);
+            }
+            return dpi / DefaultWPFDPI;
         }
 
         #endregion
