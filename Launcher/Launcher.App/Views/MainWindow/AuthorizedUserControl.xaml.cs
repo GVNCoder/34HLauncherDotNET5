480f0ba624c02e52d9e10ea7d86cf806b9e05bd5
DmitrievStanislav <dmitrievsvp@gmail.com>
2021-08-18 21:59:36 +0300

    Extracted Authorized User control

---
+++ Launcher/Launcher.App/Views/MainWindow/AuthorizedUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Launcher.Views
+{
+    /// <summary>
+    /// Interaction logic for AuthorizedUserControl.xaml
+    /// </summary>
+    public partial class AuthorizedUserControl : UserControl
+    {
+        public AuthorizedUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
