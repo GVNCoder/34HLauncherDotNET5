67ae38397f4b85fbe65bd555efd68e0864f2230a
DmitrievStanislav <dmitrievsvp@gmail.com>
2021-08-19 18:04:48 +0300

    Implemented Window Content layout (not yet)
    * Added default background image resource

---
+++ Launcher/Launcher.App/Views/MainWindow/NotificationsControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Launcher.Views
+{
+    /// <summary>
+    /// Interaction logic for NotificationsControl.xaml
+    /// </summary>
+    public partial class NotificationsControl : UserControl
+    {
+        public NotificationsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Launcher/Launcher.App/Views/MainWindow/WindowContentControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Launcher.Views
+{
+    /// <summary>
+    /// Interaction logic for WindowContentControl.xaml
+    /// </summary>
+    public partial class WindowContentControl : UserControl
+    {
+        public WindowContentControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
