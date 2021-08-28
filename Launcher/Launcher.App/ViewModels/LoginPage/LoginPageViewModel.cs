bcd1ddacf9983e8537417af3ce04f9a16bcf8c67
DmitrievStanislav <dmitrievsvp@gmail.com>
2021-08-28 21:30:12 +0300

    Update LoginPageViewModel.cs

--- Launcher/Launcher.App/ViewModels/LoginPage/LoginPageViewModel.cs
+++ Launcher/Launcher.App/ViewModels/LoginPage/LoginPageViewModel.cs
@@ -1,9 +1,9 @@
-﻿using System.Diagnostics;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Input;
 
 using Launcher.Commands;
 using Launcher.Utilities;
+
 using Zlo4NET.Api.Service;
 
 namespace Launcher.ViewModels
