a58f57245e175cd59bc88a545bd77f83a4e536db
DmitrievStanislav <dmitrievsvp@gmail.com>
2021-08-30 21:05:28 +0300

    Added a new one project called UpdateTool
    * It is creates a zip archive for update and prints result md5 hash

---
+++ Launcher/UpdateTool/Tool.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Security.Cryptography;
+
+namespace UpdateTool
+{
+    public static class Tool
+    {
+        private const string SourceDirectoryName = "Source";
+        private static readonly string DestinationFileName = Path.Combine("Output", "update.zip");
+
+        public static void  Main(string[] args)
+        {
+            ZipFile.CreateFromDirectory(SourceDirectoryName, DestinationFileName, CompressionLevel.Optimal, false);
+
+            using (var fileSteam = File.Open(DestinationFileName, FileMode.Open, FileAccess.Read))
+            using (var md5 = MD5.Create())
+            {
+                var updateHashBytes = md5.ComputeHash(fileSteam);
+                var encodedUpdateHash = BitConverter.ToString(updateHashBytes)
+                    .Replace("-", string.Empty)
+                    .ToLower();
+
+                // report
+                Console.WriteLine($"Output hash: {encodedUpdateHash}");
+            }
+
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
